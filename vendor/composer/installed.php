60fe3840444e764835c55c53bfd3a1ef7982ea57
ITerNik <ternik2705@list.ru>
2024-04-29 17:53:47 +0300

    Added composer configurations

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iternik/textlook-media',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'efc28b6c749229f866fb03951586373da00d399e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fproject/php-jwt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '91047b202bbe7d966e8fce67ab16ebca8bdcb6b7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fproject/php-jwt',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'iternik/textlook-media' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'efc28b6c749229f866fb03951586373da00d399e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
